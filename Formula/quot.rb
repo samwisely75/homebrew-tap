eb54da8780a630b5d6cd43c2e81c703f953b2a4f
samwisely75 <samwisely75@gmail.com>
2025-06-30 12:32:30 +0900

    Update quot formula to v0.1.5 with correct SHA256 checksums

--- Formula/quot.rb
+++ Formula/quot.rb
@@ -1,21 +1,21 @@
 class Quot < Formula
   desc "A fast and flexible command-line tool that converts text input into escaped string literals"
   homepage "https://github.com/samwisely75/quot"
-  version "0.1.4"
+  version "0.1.5"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/samwisely75/quot/releases/download/v0.1.4/quot-macos-x64"
-      sha256 "8e381225e1dd4cd4d8cfbf3f42b1bbdfb3b71dfb96eeeeb6fc70f4876701c8de"
+      url "https://github.com/samwisely75/quot/releases/download/v0.1.5/quot-macos-x64"
+      sha256 "dc71406d960a37749b3fbfd96a828aef175e3419542e61fd47b159677f567ac6"
 
       def install
         bin.install "quot-macos-x64" => "quot"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/samwisely75/quot/releases/download/v0.1.4/quot-macos-arm64"
-      sha256 "d4929de12b2431a2222583dd156ecc45ff4021e0eb2523c715a2b6dc8e163e6f"
+      url "https://github.com/samwisely75/quot/releases/download/v0.1.5/quot-macos-arm64"
+      sha256 "b1087528d52149cdd0a3b17fbf17d3ad35b6cd1b04fbdcb6133d48fc4569f2d2"
 
       def install
         bin.install "quot-macos-arm64" => "quot"
